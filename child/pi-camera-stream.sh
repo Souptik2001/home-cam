79e7253c92b57429ce0eb8b2c2cb29a21a5fd649
Souptik Datta <souptikdatta2001@gmail.com>
2026-06-12 22:23:03 +0530

    fix
    Signed-off-by: Souptik Datta <souptikdatta2001@gmail.com>

--- child/pi-camera-stream.sh
+++ child/pi-camera-stream.sh
@@ -18,7 +18,7 @@ sleep 2
 exec /bin/bash -lc '
 rpicam-vid -t 0 --inline --codec h264 --mode 2304:1296 --width 1280 --height 720 --framerate 30 --intra 30 -o - | \
 ffmpeg -use_wallclock_as_timestamps 1 -fflags +genpts -f h264 -r 30 -i - \
--filter_complex "[0:v]split=2[vhigh][vlow];[vlow]scale=640:360[vlowout]" \
--map "[vhigh]" -c:v libx264 -preset ultrafast -tune zerolatency -g 30 -f rtsp -rtsp_transport tcp rtsp://localhost:8554/high \
+-filter_complex "[0:v]scale=640:360[vlowout]" \
+-map 0:v:0 -c:v copy -f rtsp -rtsp_transport tcp rtsp://localhost:8554/high \
 -map "[vlowout]" -c:v libx264 -preset ultrafast -tune zerolatency -g 10 -f rtsp -rtsp_transport tcp rtsp://localhost:8554/low
 '
